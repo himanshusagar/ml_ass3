2265b15ed1577349f640c7d660a2ced7ec74bb33
himanshusagar <himanshu14046@iiitd.ac.in>
2017-10-27 12:47:15 +0530

    plot

--- bonus/architectures.py
+++ bonus/architectures.py
@@ -1,15 +1,14 @@
 from sklearn.neural_network import MLPClassifier
 
-from src.alpha import X_train
 from utility import dataset_creater
 from utility.scores import accuracy_score
 
 X_train, y_train = dataset_creater.loadIT("train")
 X_valid, y_valid = dataset_creater.loadIT("valid")
 
-mlp = MLPClassifier(hidden_layer_sizes=(50, 100,), max_iter=50, alpha=1e-4,
+mlp = MLPClassifier(hidden_layer_sizes=(500,), max_iter=50, alpha=1e-4,
                     solver='adam', verbose=10, tol=1e-4, random_state=1,
-                    activation='logistic'
+                    activation='relu'
                     )
 
 mlp.fit(X_train, y_train)
--- models/model_tester.py
+++ models/model_tester.py
@@ -5,19 +5,20 @@ from sklearn.model_selection import train_test_split
 
 from utility import dataset_creater
 
+
+X_test, y_test = dataset_creater.loadIT("test")
+X_valid , y_valid = dataset_creater.loadIT("valid")
+
+# X_raw, y = dataset_creater.loadSmallbset();
 #
-# X_test, y_test = dataset_creater.loadIT("test")
-# X_valid , y_valid = dataset_creater.loadIT("valid")
+# X = np.zeros((X_raw.shape[0], 784))
+# for i in xrange(np.shape(X)[0]):
+#     X[i] = X_raw[i].flatten()
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+#
 
-X_raw, y = dataset_creater.loadSmallbset();
-
-X = np.zeros((X_raw.shape[0], 784))
-for i in xrange(np.shape(X)[0]):
-    X[i] = X_raw[i].flatten()
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
-clf = joblib.load("sklearn_small_logistic_model_pkl")
+clf = joblib.load("sklearn_large_logistic_model_pkl")
 
 y_pred = clf.predict(X_test);
 iter_accuracy = accuracy_score(y_test, y_pred)
@@ -25,6 +26,6 @@ print("Acc Test", iter_accuracy)
 
 ##########
 
-# y_pred = clf.predict( X_valid )
-# iter_accuracy = accuracy_score( y_valid  ,  y_pred )
-# print("Acc Valid" , iter_accuracy)
+y_pred = clf.predict( X_valid )
+iter_accuracy = accuracy_score( y_valid  ,  y_pred )
+print("Acc Valid" , iter_accuracy)
